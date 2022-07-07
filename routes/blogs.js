3c3e25275f10f49247410b0e2568b2da0119a3d6
Jill Kidman <jillkidman@gmail.com>
2022-07-07 15:05:30 -0700

    end of day 3 assignment requirements

--- routes/blogs.js
+++ routes/blogs.js
@@ -13,8 +13,29 @@ router.get('/hello-blogs', (req, res) => {
 router.get("/all-blogs", async function (req, res, next) {
     try {
         const collection = await blogsDB().collection("blogs50")
-        const blogs50 = await collection.find({}).toArray();
-        res.send({ message: blogs50 });
+        const limit = Number(req.query.limit)
+        const skip = Number(req.query.limit) * (Number(req.query.page) - 1)
+        const sortField = req.query.sortField
+        const sortOrder = req.query.sortOrder === "ASC" ? 1 : -1;
+        const filterField = req.query.filterField
+        const filterValue = req.query.filterValue
+
+        let filterObj = {}
+        if (filterField && filterValue) {
+            filterObj = { [filterField]: filterValue }
+        }
+        let sortObj = {}
+        if (sortField && sortOrder) {
+            sortObj = { [sortField]: sortOrder }
+        }
+
+        const blogs50 = await collection
+            .find(filterObj)
+            .sort(sortObj)
+            .limit(limit)
+            .skip(skip)
+            .toArray();
+        res.json({ message: blogs50 });
     }
     catch (e) {
         res.status(500).send("Error fetching posts.")
